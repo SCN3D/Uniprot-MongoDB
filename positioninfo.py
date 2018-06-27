adbaa5bb9c321424219982d89e1e722446463d2d
scn3d <scn3d@mail.missouri.edu>
2018-06-27 02:32:50 -0500

    s1

--- positioninfo.py
+++ positioninfo.py
@@ -3,7 +3,7 @@ import json
 from acs import pos_total as acs
 def main():
     dbname = 'uniprot'
-    colname = 'table'
+    colname = 'ubiquitinTable'
     collection = functions.connectMongoDB(dbname,colname)
     write2file = []
     ##
@@ -11,12 +11,11 @@ def main():
         for ac in acs:
             result = collection.find_one({'ac':ac})
             print(ac)
-            if 'Glycyllysineisopeptide(Lys-Gly)(interchainwithG-Cterinubiquitin)' in result:
-                print(result['Glycyllysineisopeptide(Lys-Gly)(interchainwithG-Cterinubiquitin)'])
-                temp = {'AC': ac,'Positions':result['Glycyllysineisopeptide(Lys-Gly)(interchainwithG-Cterinubiquitin)']}
-                write2file.append(temp)
-            else:
-                print('None!')
+            temp = {'AC': ac}
+            for output in result:
+                print(result[output])
+                temp[output] = {output:result[output]}
+            write2file.append(temp)
         json.dump(write2file, outfile)
 if __name__== "__main__":
     main()
